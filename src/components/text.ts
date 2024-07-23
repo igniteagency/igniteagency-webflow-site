8634ed9f38ae0c6d6940427f0920fe8cfacb1c07
Sylvain Girard <sylvain97@gmail.com>
2024-07-23 16:04:56 +1000

    Add new text animations

--- src/components/text.ts
+++ src/components/text.ts
@@ -3,21 +3,53 @@ const SCRUB_TEXT = document.querySelectorAll('[data-scrub]');
 export function textAnimation() {
   document.querySelectorAll('[gsap-letter-stagger]').forEach((elem: Element) => {
     let typeSplit = new window.SplitType(elem, {
-      types: 'lines, words, chars',
+      types: 'words, lines, chars',
       tagName: 'span',
     });
 
+    const duration = parseFloat((elem as HTMLElement).getAttribute('data-duration') || '1');
+    const stagger = parseFloat((elem as HTMLElement).getAttribute('data-stagger') || '0.05');
+    const trigger = (elem as HTMLElement).getAttribute('data-trigger-start') || 'center';
+
     window.gsap.from(typeSplit.chars, {
       y: '100%',
-      opacity: 0,
-      duration: parseFloat((elem as HTMLElement).getAttribute('data-duration') || '0.3'),
+      duration: duration,
       ease: 'expo.out',
-      stagger: parseFloat((elem as HTMLElement).getAttribute('data-stagger') || '0.05'),
+      stagger: stagger,
       scrollTrigger: {
         trigger: elem,
-        start: 'top center',
+        start: `top ${trigger}`,
       },
     });
+  });
+
+  document.querySelectorAll('[gsap-line-stagger]').forEach((elem) => {
+    if (elem instanceof HTMLElement) {
+      let lineSplit = new window.SplitType(elem, {
+        types: 'lines',
+        tagName: 'span',
+      });
+
+      const duration = parseFloat((elem as HTMLElement).getAttribute('data-duration') || '0.3');
+      const trigger = (elem as HTMLElement).getAttribute('data-trigger-start') || 'center';
+
+      lineSplit.lines?.forEach((line) => {
+        if (line instanceof HTMLElement) {
+          window.gsap.from(line, {
+            y: '100%',
+            opacity: 0,
+            duration: duration,
+            ease: 'expo.out',
+            scrollTrigger: {
+              trigger: line,
+              start: 'top 80%',
+              end: `top ${trigger}`,
+              toggleActions: 'play none none none',
+            },
+          });
+        }
+      });
+    }
   });
 
   SCRUB_TEXT.forEach((char) => {
