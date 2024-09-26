e97405eab4446a32e1445a5bd1dc05e2cec3d2a9
Sylvain Girard <sylvain97@gmail.com>
2024-09-26 15:09:23 +1000

    Add link hover state

--- src/components/cursor.ts
+++ src/components/cursor.ts
@@ -1,12 +1,32 @@
 export function cursorMove() {
   const cursor = document.querySelector('.cursor');
+  const links = document.querySelectorAll('a');
 
   document.addEventListener('mousemove', (e) => {
     window.gsap.to(cursor, {
       x: e.clientX,
       y: e.clientY,
-      duration: 0.1,
+      duration: 0.05,
       ease: 'power2.out',
+    });
+  });
+
+  // Add hover effect for links
+  links.forEach((link) => {
+    link.addEventListener('mouseenter', () => {
+      window.gsap.to(cursor, {
+        scale: 1.5,
+        duration: 0.2,
+        ease: 'power2.out',
+      });
+    });
+
+    link.addEventListener('mouseleave', () => {
+      window.gsap.to(cursor, {
+        scale: 1,
+        duration: 0.2,
+        ease: 'power2.out',
+      });
     });
   });
 }
