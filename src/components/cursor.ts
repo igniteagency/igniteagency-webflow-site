fde07fdeef6ebbf1ede040a98ea5c34f5ec1efeb
Sylvain Girard <sylvain97@gmail.com>
2024-12-05 17:07:19 +1100

    change cursor implementation

--- src/components/cursor.ts
+++ src/components/cursor.ts
@@ -1,20 +1,37 @@
-export function cursorMove() {
-  const cursor = document.querySelector('.cursor');
-  const links = document.querySelectorAll('a');
+const TIP_LINKS = document.querySelectorAll('[data-cursor]');
+const HOVER_LINKS = document.querySelectorAll('a');
+const CURSOR = document.querySelectorAll('.cursor');
 
-  document.addEventListener('mousemove', (e) => {
-    window.gsap.to(cursor, {
-      x: e.clientX,
-      y: e.clientY,
-      duration: 0.05,
-      ease: 'power2.out',
+export function cursorMove() {
+  gsap.set(CURSOR, { xPercent: -50, yPercent: -50 });
+
+  let xTo = gsap.quickTo(CURSOR, 'x', { duration: 0.2, ease: 'power3' });
+  let yTo = gsap.quickTo(CURSOR, 'y', { duration: 0.2, ease: 'power3' });
+
+  window.addEventListener('mousemove', (e) => {
+    xTo(e.clientX);
+    yTo(e.clientY);
+  });
+
+  TIP_LINKS.forEach((link) => {
+    let text = link.getAttribute('data-cursor');
+
+    link.addEventListener('mouseenter', () => {
+      gsap.to('.cursor p', {
+        scrambleText: {
+          text: text,
+          chars: 'lowercase',
+          speed: 2,
+        },
+        duration: 0.8,
+      });
     });
   });
 
   // Add hover effect for links
-  links.forEach((link) => {
+  HOVER_LINKS.forEach((link) => {
     link.addEventListener('mouseenter', () => {
-      window.gsap.to(cursor, {
+      window.gsap.to(CURSOR, {
         scale: 1.5,
         duration: 0.2,
         ease: 'power2.out',
@@ -22,7 +39,7 @@ export function cursorMove() {
     });
 
     link.addEventListener('mouseleave', () => {
-      window.gsap.to(cursor, {
+      window.gsap.to(CURSOR, {
         scale: 1,
         duration: 0.2,
         ease: 'power2.out',
